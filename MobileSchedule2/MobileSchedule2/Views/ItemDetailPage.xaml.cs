f41e5a4bc648a1beba45a8c8ad2f02e940b53cbf
Doskarayev Nurlan <n.doskaraev@agrostream.net>
2019-02-19 14:41:43 +0600

    Add project files.

---
+++ MobileSchedule2/MobileSchedule2/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MobileSchedule2.Models;
+using MobileSchedule2.ViewModels;
+
+namespace MobileSchedule2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
